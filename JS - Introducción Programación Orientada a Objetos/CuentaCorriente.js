3a741d799712923265506f60fec3783b75a8b65c
Luciano Souberbielle <lucianosouberbielle@mi.unc.edu.ar>
2022-08-11 13:51:29 -0300

    Cuenta corriente, cliente, index y archivo json

---
+++ JS - Introducción Programación Orientada a Objetos/CuentaCorriente.js
@@ -0,0 +1,37 @@
+export class CuentaCorriente    //clase
+{
+    cliente;
+    numero;
+    agencia;
+    #saldo;
+
+    constructor() {             //constructor
+        this.cliente = null;
+        this.numero = '';
+        this.agencia = '';
+        this.#saldo = 0;
+    }
+
+    depositoEnCuenta(valor) {  //métodos
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+        valor = 200;
+        valor = valor*1000;
+    }
+}
